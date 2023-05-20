e23da281e343af9b41ccad094074448da3d00ad9
cor22982 <cor22982@uvg.edu.gt>
2023-05-19 19:22:14 -0600

    Primer Comit

---
+++ Lab9/Lab9Precalificador.asm
@@ -0,0 +1,5 @@
+; Universidad del Valle de Guatemala
+; Mathew Cordero, 22982 - Jose de Leon,  22809 Grupo 4
+; Descripcion:
+; El programa realiza una inspeccion de los impuestos ISR a tributar y el regimen para el proximo año fiscal
+; 10/05/2023
